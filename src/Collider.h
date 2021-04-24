3a63f42d802354b4c5d69ef671c6a5197a3db257
dh00mk3tu <cyberhybird@hotmail.com>
2021-04-24 13:49:31 +0530

    This commit is made by Auto-Git. Commit-201, Saturday 24 April 2021 10:15:08 AM IST

--- src/Collider.h
+++ src/Collider.h
@@ -4,10 +4,24 @@
 
 class Collider {
     public: 
-        Collider();
+        Collider(sf::RectangleShape& body);
         ~Collider();
+
+        void Move(float dx, float dy) {
+            body.Move(dx, dy);
+        }
+
+        bool CheckCollider(Collider& other, float push);
+        
+        sf::Vector2f GetPosition() {
+            return body.getPosition();
+        }
+
+        sf::Vector2f GetHalfSize() {
+            return body.getSize()/2.0f;
+        }
 
     private: 
         sf::RectangleShape& body;
-        
+
 };
