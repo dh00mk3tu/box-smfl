950d90bedbab2daf568a307a24f57ae4e4553a70
dh00mk3tu <cyberhybird@hotmail.com>
2021-04-24 13:47:20 +0530

    This commit is made by Auto-Git. Commit-199, Saturday 24 April 2021 10:15:08 AM IST

--- src/Collider.h
+++ src/Collider.h
@@ -1,8 +1,13 @@
 #pragma once //
 #include <SFML/Graphics.hpp>
 
+
 class Collider {
     public: 
         Collider();
         ~Collider();
+
+    private: 
+        sf::RectangleShape& body;
+        
 };
